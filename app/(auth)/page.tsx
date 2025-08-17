dc4481e1d6093d585ea87762fd2e01e4ce4083ae
Rohitchand-01 <rohitchand010904@gmail.com>
2025-08-18 03:01:51 +0530

    Login Page

---
+++ app/(auth)/page.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import styles from './page.module.scss'
+import Image from 'next/image'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div className={styles.main_container}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <Image
+            src='/images/login_card.png'
+            alt=''
+            width={394}
+            height={256}
+            className={styles.image}
+            quality={100}
+          />
+          <div className={styles.title}>
+            <p className={styles.heading}>Manage your contacts with ease.</p>
+            <p className={styles.subheading}>
+              Sign in to organize, tag, and track your leads, all in one place.
+            </p>
+          </div>
+          <div className={styles.actions}>
+            <div className={styles.button}>
+              <Image
+                src='/images/Google.svg'
+                alt='Google Logo'
+                width={24}
+                height={24}
+                className={styles.button_icon}
+              />
+              <p className={styles.button_text}>Continue with Google</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default page
